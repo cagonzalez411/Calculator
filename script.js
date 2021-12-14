e8858f5faf37d00f3bbc8d80405ccd70d4afcd38
Carlos G <cagonzalez411@gmail.com>
2021-12-14 02:52:56 -0500

    Got display to chnage if there was a string in the display

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 const display = document.querySelector('#display')
+const subdisplay = document.querySelector('#subdisplay')
 const digits = document.querySelectorAll('.digit')
 const clear = document.querySelector('#clear')
 const operators = document.querySelectorAll('.operator')
 const equals = document.querySelector('#equals')
-let currentDisplay = display.textContent
-let operation = null
-let operationSign = null
-let firstEle = null
-let secondEle = null
-let input = false
 
 function add(a,b) {
     return a + b
@@ -25,86 +20,29 @@ function multiply(a, b) {
 function divide(a, b) {
     if (b == 0) {
         alert('Error: Dividing by zero')
-        return
+        return null
     }
     return a / b
 }
 
 function operate(operator, a, b) {
-    if (operation) {
-        return operator(a, b)
-    }
+    return operator(a, b)
 }
 
-function clearContent() {
-    display.textContent = 0
-    subdisplay.textContent = '\xa0'
-    operation = ''
-    operationSign = ''
-    firstEle = 0
-    secondEle = 0
-    input = false
-}
-
-function changeDisplay(textContent) {
-    if (display.textContent === '0') {
-        display.textContent = textContent
-        currentDisplay = +display.textContent
+function changeDisplay(number) {
+    if (+display.textContent == 0 || Number.isNaN(+display.textContent)) {
+        display.textContent = number
     } else {
-        display.textContent += textContent
-        currentDisplay = +display.textContent
+        display.textContent += number
     }
 }
 
-function getOperator(operatorChosen) {
-    switch (operatorChosen) {
-        case '/':
-            operation = divide
-            operationSign = '/'
-            break
-        case 'x':
-            operation = multiply
-            operationSign = 'x'
-            break
-        case '-':
-            operation = subtract
-            operationSign = '-'
-            break
-        case '+':
-            operation = add
-            operationSign = '+'
-            break
-    }
-}
-
-digits.forEach(e => {
-    e.addEventListener('click', function() {
-        changeDisplay(e.textContent)
-        input = true
+digits.forEach(digit => {
+    digit.addEventListener('click', function () {
+        changeDisplay(digit.textContent)
     })
 })
 
-operators.forEach(operator => {
-    operator.addEventListener('click', function() {
-        getOperator(operator.textContent)
-        firstEle = +display.textContent
-        subdisplay.textContent = +firstEle + ` ${operationSign} `
-        display.textContent = 0
-    })
-})
-
-equals.addEventListener('click', function() {
-    if (!input || !operation) return
-    let result = operate(operation, +firstEle, +display.textContent)
-    if (typeof(result) == 'number') {
-        let secondEle = display.textContent
-        let resultRounded = Math.round(result * 10000)/10000
-        display.textContent = resultRounded
-        subdisplay.textContent = +firstEle + ` ${operationSign} ` + +secondEle + ' ='
-    }
-    
-})
 
 
 
-clear.addEventListener('click', clearContent)
