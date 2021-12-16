bcaf11e6180208ca45e287fde42f4ca530f19e6b
Carlos G <cagonzalez411@gmail.com>
2021-12-16 01:30:48 -0500

    Edited

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const digits = document.querySelectorAll('.digit')
 const clear = document.querySelector('#clear')
 const operators = document.querySelectorAll('.operator')
 const equals = document.querySelector('#equals')
+let firstEle = null
+let secondEle = null
+let wasOperationChosen = null 
+let operatorChosen = null
+let operateResult = null
+let evaluated = false
+
 
 function add(a,b) {
     return a + b
@@ -29,11 +36,43 @@ function operate(operator, a, b) {
     return operator(a, b)
 }
 
+function chooseOperation(operator) {
+    switch (operator) {
+        case '/':
+            operatorChosen = divide
+            break
+        case 'x':
+            operatorChosen = multiply
+            break
+        case '+':
+            operatorChosen = add
+            break
+        case '-':
+            operatorChosen = subtract
+            break
+    }
+    display.textContent = 0
+}
+
+function clearFunction() {
+    firstEle = null
+    secondEle = null
+    wasOperationChosen = null 
+    operatorChosen = null
+    display.textContent = 0
+
+}
+
 function changeDisplay(number) {
     if (+display.textContent == 0 || Number.isNaN(+display.textContent)) {
         display.textContent = number
     } else {
         display.textContent += number
+    }
+    if (!wasOperationChosen) {
+        firstEle = +display.textContent
+    } else {
+        secondEle = +display.textContent
     }
 }
 
@@ -43,6 +82,23 @@ digits.forEach(digit => {
     })
 })
 
+operators.forEach(operator => {
+    operator.addEventListener('click', function() {
+        wasOperationChosen = true
+        chooseOperation(operator.textContent)
+        subdisplay.textContent = firstEle
+    })
+})
+
+equals.addEventListener('click', function() {
+    operateResult = operate(operatorChosen, firstEle, secondEle)
+    display.textContent = operateResult
+    evaluated = true
+})
+
+clear.addEventListener('click', () => {
+    clearFunction()
+})
 
 
 
